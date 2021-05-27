41ab5d5537f0ae2b9e951ea4f9835cf8a2678d76
Farhan Ali <fahanali3193@gmail.com>
2021-05-26 20:50:05 -0700

    Initial set up. Fleshing out file structure

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>HELLO</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const servingPath = path.join(__dirname, '..', 'client', 'dist')
+
+app.use(express.static(servingPath));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port $${port}`);
+})

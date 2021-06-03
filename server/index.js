ae0bf5f2f0ce9dd9b6254eb505be393567d84a8a
Jason Carr <carrjsn@gmail.com>
2021-06-02 20:07:02 -0700

    install dotenv and updated server to include .env variables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+const gitToken = process.env.GIT_API_TOKEN;
 
 const app = express();
 const servingPath = path.join(__dirname, '..', 'client', 'dist')
 
 app.use(express.static(servingPath));
+
+console.log(gitToken);
 
 const port = 3000;
 app.listen(port, () => {

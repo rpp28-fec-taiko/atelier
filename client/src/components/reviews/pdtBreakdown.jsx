6fb2b6fee0b637253ff823051b89ad0d7c72e60b
Farhan Ali <fahanali3193@gmail.com>
2021-06-04 13:26:37 -0700

    Created reviews structure with sub components using grid. Modified tests. Added css

---
+++ client/src/components/reviews/pdtBreakdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PdtBreakdown = () => {
+  return (
+    <div className='reviews-pdt-breakdown'> PdtBreakdown </div>
+  );
+};
+
+export default PdtBreakdown;
---
+++ client/src/components/reviews/reviewsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewsList = () => {
+  return (
+    <div className='reviews-list'>
+      REVIEWS LIST
+    </div>
+  );
+};
+
+export default ReviewsList;

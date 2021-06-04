c4cfe095a5cbfea8b3a7a60dd093444c38801a6c
Farhan Ali <fahanali3193@gmail.com>
2021-06-03 20:53:02 -0700

    Added dummy test for reviews. Made test fn in app test to be a fn expression to avoid linting error

--- test/componentsTest/appTest/app.test.js
+++ test/componentsTest/appTest/app.test.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../../client/src/components/app/app.jsx';
 
-function sum(a, b) {
+let sum = function (a, b) {
   return a + b;
-}
+};
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
---
+++ test/componentsTest/reviewsTest/reviews.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Reviews from '../../../client/src/components/reviews/reviews.jsx';
+
+test('checks that Reviews component is rendering', () => {
+  const wrapper = shallow(<Reviews />);
+  expect(wrapper.text()).toEqual('REVIEWS');
+});

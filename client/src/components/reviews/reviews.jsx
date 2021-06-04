89bc7791c063c7f24e8442975f86099e7d3ca5ca
Farhan Ali <fahanali3193@gmail.com>
2021-06-03 20:38:44 -0700

    Created empty reviews component

---
+++ client/src/components/reviews/reviews.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Reviews extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        REVIEWS
+      </div>
+    );
+  }
+}
+
+export default Reviews;

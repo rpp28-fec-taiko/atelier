d41228b961a678db4d4f1392c572b3381a5bbdfd
Farhan Ali <fahanali3193@gmail.com>
2021-06-04 19:33:47 -0700

    Added sub items to review tile component.

--- client/src/components/reviews/reviewTile.jsx
+++ client/src/components/reviews/reviewTile.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const ReviewTile = () => {
   return (
-    <div className='reviews-tile'> REVIEW TILE </div>
+    <div className='reviews-tile'>
+      <div className='reviews-tile-stars-date'>
+        <div className='reviews-tile-stars'>STARS</div>
+        <div className='reviews-tile-usernameDate'>USERNAME, DATE</div>
+      </div>
+      <div>SUMMARY</div>
+      <div>BODY</div>
+      <div>I RECOMMEND THIS PDT</div>
+      <div>RESPONSE</div>
+      <div>HELPFUL YES OR NO | REPORT</div>
+    </div>
   );
 };
 

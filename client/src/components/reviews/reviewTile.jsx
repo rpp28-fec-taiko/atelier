7d78907ac6855c554b1a20f85b6dcffabac2f340
Farhan Ali <fahanali3193@gmail.com>
2021-06-06 12:04:19 -0700

    Made date human readable

--- client/src/components/reviews/reviewTile.jsx
+++ client/src/components/reviews/reviewTile.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Stars from '../stars/stars.jsx';
 
 const ReviewTile = ({ review }) => {
+  console.log('date', new Date(review.date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }));
   return (
     <div className='reviews-tile'>
       <div className='reviews-tile-stars-date'>
         <div className='reviews-tile-stars'> <Stars size={40} rating={4.39}/> </div>
-        <div className='reviews-tile-usernameDate'>{review.reviewer_name} , {review.date}</div>
+        <div className='reviews-tile-usernameDate'>{review.reviewer_name}, {new Date(review.date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
       </div>
       <div>SUMMARY {review.summary}</div>
       <div>BODY {review.body}</div>

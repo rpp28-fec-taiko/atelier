40a32bf31a277427abc3d369a128fda67acf564b
Farhan Ali <fahanali3193@gmail.com>
2021-06-06 11:29:45 -0700

    Fixed app.jsx and installed css loader

--- client/src/components/app/app.jsx
+++ client/src/components/app/app.jsx
@@ -4,7 +4,6 @@ import Overview from '../productOverview/Overview.jsx';
 import QAndA from '../qAndA/qAndA.jsx';
 import RelatedItems from '../relatedItems/RelatedItems.jsx';
 
-<<<<<<< HEAD
 class App extends React.Component {
   constructor (props) {
     super(props);
@@ -27,23 +26,12 @@ class App extends React.Component {
     return (
       <div className='app'>
         <Overview />
-        <div>Related Products</div>
+        <RelatedItems />
         <QAndA />
         <Reviews reviews={this.state.reviews}/>
       </div>
     );
   }
-=======
-const App = () => {
-  return (
-    <div className='app'>
-      <Overview />
-      <RelatedItems/>
-      <QAndA />
-      <Reviews />
-    </div>
-  );
->>>>>>> main
 };
 
 export default App;

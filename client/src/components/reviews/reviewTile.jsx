7c4ebeb71f9be1a74c8759fe1ddeb2f9f20d9287
Farhan Ali <fahanali3193@gmail.com>
2021-06-06 12:30:20 -0700

    Made review summary bigger.

--- client/src/components/reviews/reviewTile.jsx
+++ client/src/components/reviews/reviewTile.jsx
@@ -6,10 +6,10 @@ const ReviewTile = ({ review }) => {
   return (
     <div className='reviews-tile'>
       <div className='reviews-tile-stars-date'>
-        <div className='reviews-tile-stars'> <Stars size={40} rating={4.39}/> </div>
+        <div className='reviews-tile-stars'> <Stars size={24} rating={review.rating}/> </div>
         <div className='reviews-tile-usernameDate'>{review.reviewer_name}, {new Date(review.date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
       </div>
-      <div>SUMMARY {review.summary}</div>
+      <div className='reviews-tile-summary'> {review.summary} </div>
       <div>BODY {review.body}</div>
       {review.recommend ? <div>I RECOMMEND THIS PDT </div> : null}
       {review.response}

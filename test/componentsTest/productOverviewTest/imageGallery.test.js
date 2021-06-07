5f862064c89ef4435c40753d19d6e23158f03c2f
Jason Carr <carrjsn@gmail.com>
2021-06-06 18:37:48 -0700

    fix test names for Styles and ImageGallery

--- test/componentsTest/productOverviewTest/imageGallery.test.js
+++ test/componentsTest/productOverviewTest/imageGallery.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ImageGallery from '../../../client/src/components/productOverview/ImageGallery.jsx';
 
-test('checks that Cart component renders correctly', () => {
+test('checks that Image Gallery component renders correctly', () => {
   const wrapper = shallow(<ImageGallery />);
   expect(wrapper.text()).toContain('Image Gallery');
 });

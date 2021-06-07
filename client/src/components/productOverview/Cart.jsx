381e75d0449e171187da677977580913f80c31c9
Jason Carr <carrjsn@gmail.com>
2021-06-07 14:48:53 -0700

    Added onChange handlers for quantity and size selections in cart

--- client/src/components/productOverview/Cart.jsx
+++ client/src/components/productOverview/Cart.jsx
@@ -3,19 +3,37 @@ import React from 'react';
 class Cart extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      size: null,
+      quantity: null
+    }
+  }
+
+  handleSizeChange(e) {
+    this.setState({size: e.target.value}, () => console.log(this.state));
+  }
+
+  handleQuantityChange(e) {
+    this.setState({quantity: e.target.value}, () => console.log(this.state));
   }
 
   render() {
     return (
       <div className='cart'>
         <div className='size'>
-          <select>
-            <option defaultValue="Select Size">Select Size</option>
+          <select onChange={this.handleSizeChange.bind(this)}>
+            <option defaultValue='Select Size'>Select Size</option>
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
           </select>
         </div>
 
         <div className='quantity'>
-          <select>
+          <select onChange={this.handleQuantityChange.bind(this)}>
             <option defaultValue="Select Quantity">Qty</option>
             <option>1</option>
             <option>2</option>

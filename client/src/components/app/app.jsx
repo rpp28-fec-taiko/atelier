1698dea6066ef2bf1dd478a51ffcb30d374843a5
Farhan Ali <fahanali3193@gmail.com>
2021-06-08 15:45:27 -0700

    Changed how reviews were being fetched. Also implemented sort by date feature

--- client/src/components/app/app.jsx
+++ client/src/components/app/app.jsx
@@ -9,62 +9,72 @@ class App extends React.Component {
     super(props);
     this.state = {
       productId: '22161',
+      totalReviews: [],
       currentReviews: [],
       nextReviews: [],
-      reviewPage: 1,
       reviewCriteria: 'relevant',
-      totalReviews: 0,
+      noOfReviews: 0,
       avgRating: 0
     };
   }
 
   sortReviews = (criteria) => {
     if (criteria === 'newest') {
-      var newCurrentReviews = this.state.currentReviews.slice().sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-      // console.log('newCurrentRev', newCurrentReviews);
+      let totalReviews = this.state.totalReviews.slice().sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      let idx = this.state.currentReviews.length;
+      let currentReviews = totalReviews.slice(0, idx);
+      let nextReviews = totalReviews.slice(idx, idx + 2);
       this.setState((prevState) => ({
-        currentReviews: [...newCurrentReviews],
+        totalReviews,
+        currentReviews,
+        nextReviews,
         reviewCriteria: criteria
       }), () => console.log('sorted', this.state))
     }
   }
 
-  get2Reviews = (page) => {
-    return fetch (`http://localhost:3000/reviews?page=${this.state.reviewPage}&count=2&productId=${this.state.productId}`)
+  getAllReviews = () => {
+    return fetch(`http://localhost:3000/allReviews?productId=${this.state.productId}`)
     .then((resp) => resp.json())
-    .then((reviews) => {
-      // console.log('reviews', reviews);
-      this.setState((prevState) =>  {
-        return {
-          currentReviews: [...prevState.currentReviews, ...prevState.nextReviews],
-          nextReviews: reviews,
-          reviewPage: prevState.reviewPage + 1
+    .then((allReviews) => {
+      // console.log('all reviews', allReviews)
+      let ratings = {};
+      allReviews.forEach((review) => {
+        let star = review.rating;
+        if (ratings[star] === undefined) {
+          ratings[star] = 1;
+        } else {
+          ratings[star] += 1
         }
-      }, () => this.sortReviews(this.state.reviewCriteria));
+      })
+      let sumOfStars = 0;
+      for (let key in ratings) {
+        sumOfStars += ratings[key] * Number(key)
+      }
+      // console.log('sum', sumOfStars, ratings)
+      let avgRating = sumOfStars / allReviews.length;
+      //DO SORTING HERE SO THAT INITIAL RESULTS ARE ALSO SORTED
+      this.setState({
+        totalReviews: allReviews,
+        noOfReviews: allReviews.length,
+        avgRating,
+        currentReviews: allReviews.slice(0, 2),
+        nextReviews: allReviews.slice(2, 4)
+      }, () => console.log('avg review state', this.state))
     })
     .catch((err) => {
-      console.log('ERROR GETTING 2 ADDITIONAL REVIEWS', err)
+      console.log('ERROR GETTING ALL REVIEWS', err);
     })
   }
 
-  getInitialReviews = () => {
-    return fetch (`http://localhost:3000/reviews?page=${this.state.reviewPage}&count=4&productId=${this.state.productId}`)
-    .then((resp) => resp.json())
-    .then((reviews) => {
-      // console.log('reviews', reviews);
-      let currentReviews = reviews.slice(0, 2);
-      let nextReviews = reviews.slice(2);
-      this.setState((prevState) => {
-        return {
-          currentReviews,
-          nextReviews,
-          reviewPage: prevState.reviewPage + 2
-        };
-      }, () => console.log('initial reviews state', this.state));
-    })
-    .catch((err) => {
-      console.log('ERROR GETTING INITIAL REVIEWS', err)
-    })
+  get2Reviews = (page) => {
+    let idx = this.state.currentReviews.length + 2;
+    let currentReviews = this.state.totalReviews.slice(0, idx);
+    let nextReviews = this.state.totalReviews.slice(idx, idx + 2);
+    this.setState((prevState) => ({
+      currentReviews,
+      nextReviews
+    }), () => console.log('2 morereviews state', this.state))
   }
 
   increaseReviewHelpfulnesss = (reviewId) => {
@@ -110,38 +120,8 @@ class App extends React.Component {
 
   }
 
-  getAllReviews = () => {
-    return fetch(`http://localhost:3000/allReviews?productId=${this.state.productId}`)
-    .then((resp) => resp.json())
-    .then((allReviews) => {
-      // console.log('all reviews', allReviews)
-      let ratings = {};
-      allReviews.forEach((star) => {
-        if (ratings[star] === undefined) {
-          ratings[star] = 1;
-        } else {
-          ratings[star] += 1
-        }
-      })
-      let sumOfStars = 0;
-      for (let key in ratings) {
-        sumOfStars += ratings[key] * Number(key)
-      }
-      // console.log('sum', sumOfStars, ratings)
-      let avgRating = sumOfStars / allReviews.length;
-      this.setState({
-        totalReviews: allReviews.length,
-        avgRating
-      }, () => console.log('avg review state', this.state))
-    })
-    .catch((err) => {
-      console.log('ERROR GETTING ALL REVIEWS', err);
-    })
-  }
-
   componentDidMount () {
-    return this.getInitialReviews()
-    .then(() => this.getAllReviews());
+    this.getAllReviews();
   }
 
   render () {
@@ -150,7 +130,7 @@ class App extends React.Component {
         <Overview />
         <RelatedItems />
         <QAndA />
-        <Reviews currentReviews={this.state.currentReviews} nextReviews={this.state.nextReviews} totalReviews={this.state.totalReviews} increaseReviewHelpfulnesss={this.increaseReviewHelpfulnesss} reportReview={this.reportReview} get2Reviews={this.get2Reviews} sortReviews={this.sortReviews} reviewCriteria={this.state.reviewCriteria}/>
+        <Reviews currentReviews={this.state.currentReviews} nextReviews={this.state.nextReviews} noOfReviews={this.state.noOfReviews} increaseReviewHelpfulnesss={this.increaseReviewHelpfulnesss} reportReview={this.reportReview} get2Reviews={this.get2Reviews} sortReviews={this.sortReviews} reviewCriteria={this.state.reviewCriteria}/>
       </div>
     );
   }

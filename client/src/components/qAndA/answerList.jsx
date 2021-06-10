3e3b5615ccd004b08c46222d0a545556d26aa9dd
Evan Nouis <evan.nouis@gmail.com>
2021-06-09 23:29:35 -0500

    Added AnswerList component

---
+++ client/src/components/qAndA/answerList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AnswerList = ({question}) => {
+  let answersData = [];
+
+  for (var key in question) {
+    answersData.push(question[key]);
+  }
+  let result = answersData.map((answer) =>
+    <div>
+      <div className="qAndA-list-answer-item" >A: {answer.body}</div>
+    </div>
+  );
+
+  return (
+    <div className="qAndA-answerFeed" >
+      {result}
+    </div>
+  );
+};
+
+export default AnswerList;
--- client/src/components/qAndA/qAndAList.jsx
+++ client/src/components/qAndA/qAndAList.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import Question from './question.jsx';
-
-const createAnswersList = (data) => {
-  let answersData = [];
-  for (var key in data) {
-    answersData.push(data[key]);
-  }
-  let result = answersData.map((answer) =>
-    <div>
-      <div className="qAndA-list-answer-item" >A: {answer.body}</div>
-    </div>
-  );
-  return result;
-};
+import AnswerList from './answerList.jsx';
 
 const QAndAList = ({questions}) => (
   <div className="qAndA-list">
@@ -21,7 +8,7 @@ const QAndAList = ({questions}) => (
       <div className="qAndA-list-question-item">
         Q: {item.question_body}<br />
         by {item.asker_name} | {item.question_date}
-        {createAnswersList(item.answers)}
+        <AnswerList question={item.answers} />
       </div>
       ) : null}
   </div>

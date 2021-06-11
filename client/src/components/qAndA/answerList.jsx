56cd8100e5fd89c6f6761e73a6a0111644cc95bd
Evan Nouis <evan.nouis@gmail.com>
2021-06-11 10:22:59 -0500

    Answer list components nested correctly

--- client/src/components/qAndA/answerList.jsx
+++ client/src/components/qAndA/answerList.jsx
@@ -8,14 +8,14 @@ const AnswerList = ({answers}) => {
   }
 
   let result = answersData.map((answer) =>
-    <div>
-      <div className="qAndA-list-answer-item" >A: {answers[answer].body}</div>
+    <div className="qAndA-list-answer-item">
+      <div  >A: {answers[answer].body}</div>
       {answers[answer].photos.length !== 0 ? answers[answer].photos.map((photo) =>
         <img className="qAndA-list-answer-item-picture" src={photo} ></img> ) : null}
-      <div>
-      by {answers[answer].answerer_name} - {answers[answer].date} | Helpful? yes({answers[answer].helpfulness})
-      <button >Report</button>
-      </div>
+        <div className="qAndA-list-answer-item-answerer">
+          by {answers[answer].answerer_name} - {answers[answer].date} | Helpful? yes({answers[answer].helpfulness})
+          <button >Report</button>
+        </div>
     </div>
   );
 

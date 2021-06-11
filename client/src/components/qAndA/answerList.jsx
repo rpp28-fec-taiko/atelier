37a077b8c0dfc7d62f00e03a9964b748b12fd501
Evan Nouis <evan.nouis@gmail.com>
2021-06-11 10:16:33 -0500

    All necessary data now rendering in QAndA List

--- client/src/components/qAndA/answerList.jsx
+++ client/src/components/qAndA/answerList.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-const AnswerList = ({question}) => {
+const AnswerList = ({answers}) => {
   let answersData = [];
 
-  for (var key in question) {
-    answersData.push(question[key]);
+  for (var key in answers) {
+    answersData.push(key);
   }
 
   let result = answersData.map((answer) =>
     <div>
-      <div className="qAndA-list-answer-item" >A: {answer.body}</div>
+      <div className="qAndA-list-answer-item" >A: {answers[answer].body}</div>
+      {answers[answer].photos.length !== 0 ? answers[answer].photos.map((photo) =>
+        <img className="qAndA-list-answer-item-picture" src={photo} ></img> ) : null}
+      <div>
+      by {answers[answer].answerer_name} - {answers[answer].date} | Helpful? yes({answers[answer].helpfulness})
+      <button >Report</button>
+      </div>
     </div>
   );
 
--- client/src/components/qAndA/qAndAList.jsx
+++ client/src/components/qAndA/qAndAList.jsx
@@ -7,8 +7,9 @@ const QAndAList = ({questions}) => (
     {questions.length !== 0 ? questions.results.map((item) =>
       <div className="qAndA-list-question-item">
         Q: {item.question_body}<br />
-        by {item.asker_name} | {item.question_date}
-        <AnswerList question={item.answers} />
+        by {item.asker_name} | {item.question_date} | Helpful? yes({item.question_helpfulness})
+         <button > Add Answer</button>
+        <AnswerList answers={item.answers} />
       </div>
       ) : null}
   </div>

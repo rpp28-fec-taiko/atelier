8a2b0bdbf6e9e1ab501890e1225ca8eb9ee2277b
Farhan Ali <fahanali3193@gmail.com>
2021-06-12 11:49:37 -0700

    Added key prop to qAndA widget

--- client/src/components/qAndA/qAndAList.jsx
+++ client/src/components/qAndA/qAndAList.jsx
@@ -4,8 +4,8 @@ import AnswerList from './answerList.jsx';
 const QAndAList = ({questions}) => (
   <div className="qAndA-list">
     {console.log('questions obj inside qanda: ', questions)}
-    {questions.length !== 0 ? questions.results.map((item) =>
-      <div className="qAndA-list-question-item">
+    {questions.length !== 0 ? questions.results.map((item, idx) =>
+      <div key={idx} className="qAndA-list-question-item">
         Q: {item.question_body}<br />
         by {item.asker_name} | {item.question_date}
         <AnswerList question={item.answers} />

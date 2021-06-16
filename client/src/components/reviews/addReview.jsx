25af0ff2bb533a465e8cdf2bbb90e6967b4d631a
Farhan Ali <fahanali3193@gmail.com>
2021-06-15 17:45:04 -0700

    Fixed bug in stars inside add review. Also added validation

--- client/src/components/reviews/addReview.jsx
+++ client/src/components/reviews/addReview.jsx
@@ -7,7 +7,7 @@ class AddReview extends React.Component {
     super (props);
     this.state = {
       showModal: false,
-      selectedStars: 0,
+      rating: 0,
       recommend: '',
       summary: '',
       body: '',
@@ -27,9 +27,21 @@ class AddReview extends React.Component {
 
   starsClicked = (e) => {
     // console.log('starsclicked', e.target.dataset.starnumber)
-    this.setState((prevState) => ({
-      selectedStars: Number(e.target.dataset.starnumber)
-    }))
+    this.setState((prevState) => {
+      let rating;
+      let stars = e.target.dataset.starnumber
+      console.log('stars', stars)
+      if (stars === undefined) {
+        rating = prevState.rating
+      } else {
+        rating = Number(stars)
+      }
+
+      return {
+        ...prevState,
+        rating
+      }
+    })
   }
 
   onInputChange = (e) => {
@@ -37,7 +49,7 @@ class AddReview extends React.Component {
     this.setState((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value
-    }), () => console.log('state rec', this.state))
+    }), () => console.log('state after updating recommend', this.state))
   }
 
   onCharacteristicChange = (e, id) => {
@@ -47,7 +59,35 @@ class AddReview extends React.Component {
         ...prevState.characteristics,
         [id]: e.target.value
       }
-    }), () => console.log('After chngin', this.state))
+    }), () => console.log('After chnging characteristics', this.state))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //Any mandatory fields are blank: Recommend, body, name and email are taken care of by input element validation.
+    let {rating, recommend, body, nickname, email, characteristics } = this.state;
+    // console.log('pre submit', rating, recommend, body, nickname, email, characteristics)
+      //rating = 0 || recommend = '' || body = '' || nickname = '' || email = '' || loop over char obj and see that all th required keys have a value
+    if (rating === 0) {
+      window.alert(`You must enter the following: \n Overall Rating`)
+      return;
+    }
+    // let { characteristics } = this.props;
+    // console.log('character', characteristics, this.props.characteristics);
+    let originalCharacteristicsList = Object.values(this.props.characteristics);
+    let userSelectedCharacteristics = Object.keys(characteristics);
+    console.log('OK', originalCharacteristicsList, userSelectedCharacteristics)
+
+    for (let i = 0; i < originalCharacteristicsList.length; i++) {
+      if (userSelectedCharacteristics.indexOf(String(originalCharacteristicsList[i].id)) === -1) {
+        console.log('CHECL', originalCharacteristicsList[i].id)
+        window.alert(`You must enter the following: \n Characteristics`)
+        return;
+      }
+    }
+    //Body < 50
+    //email not in correct format
+    //The images selected are invalid or unable to be uploaded.
   }
 
   render () {
@@ -65,25 +105,27 @@ class AddReview extends React.Component {
         <button type='button' onClick={this.displayModal}> ADD A REVIEW </button>
         {
           this.state.showModal ?
-            <div className='add-review-modal'>
+            <form className='add-review-modal' onSubmit={this.handleSubmit}>
               <div className='add-review-modal-main'>
                 <h3> Write Your Review </h3>
                 <h4> About {this.props.productName} </h4>
                 <div className='add-review-rating'>
                   Overall Rating *
-                  <div className='description' onClick={this.starsClicked} >
-                    <Stars size={18} rating={this.state.selectedStars} />
-                  {
-                      ['Poor', 'Fair', 'Average', 'Good', 'Great'][this.state.selectedStars - 1]
-                  }
+                  <div className='description' >
+                    <div className='stars' onClick={this.starsClicked}>
+                      <Stars size={18} rating={this.state.rating} />
+                    </div>
+                    {
+                      ['Poor', 'Fair', 'Average', 'Good', 'Great'][this.state.rating - 1]
+                    }
                   </div>
                 </div>
 
                 <div onClick={this.onInputChange}>
                   Do you recommend this product? *
-                  <input type='radio' name='recommend' id='yes' value='yes'/>
+                  <input type='radio' name='recommend' id='yes' value='yes' required/>
                   <label htmlFor='yes'> YES </label>
-                  <input type='radio' name='recommend' id='no' value='no'/>
+                  <input type='radio' name='recommend' id='no' value='no' required/>
                   <label htmlFor='no'> NO </label>
                 </div>
 
@@ -103,7 +145,7 @@ class AddReview extends React.Component {
 
                 <div className='add-review-body'>
                   <label htmlFor='body'> Review body * </label>
-                  <textarea id='body' name='body' rows={10} cols={80} minLength={50} maxLength={1000} placeholder='Why did you like the product or not?' onChange={this.onInputChange}>
+                  <textarea id='body' name='body' rows={10} cols={80} minLength={50} maxLength={1000} placeholder='Why did you like the product or not?' onChange={this.onInputChange} required>
                   </textarea>
                   <p>Minimum required characters left: { this.state.body.length >= 50 ? 'Minimum reached' : (50 - this.state.body.length) } </p>
                 </div>
@@ -112,20 +154,23 @@ class AddReview extends React.Component {
 
                 <div className='add-review-nickname'>
                   <label htmlFor='nickname'> What is your nickname *</label>
-                  <input type='text' id='nickname' name='nickname' maxLength={60} placeholder='Example: jackson11!' onChange={this.onInputChange} />
+                  <input type='text' id='nickname' name='nickname' maxLength={60} placeholder='Example: jackson11!' onChange={this.onInputChange} required />
                   <p style={{marginTop: '0'}}>For privacy reasons, do not use your full name or email address</p>
                 </div>
 
                 <div className='add-review-email'>
                   <label htmlFor='email'> Your email *</label>
-                  <input type='email' id='email' name='email' maxLength={60} placeholder='Example: jackson11@email.com' onChange={this.onInputChange} />
+                  <input type='email' id='email' name='email' maxLength={60} placeholder='Example: jackson11@email.com' onChange={this.onInputChange} required />
                   <p style={{marginTop: '0'}}>For authentication reasons, you will not be emailed</p>
                 </div>
 
-                <div> Submit review </div>
+                <div className='add-review-submit'>
+                  <button type='submit' style={{cursor: 'pointer'}}>  SUBMIT REVIEW </button>
+                </div>
+
                 <button type='button' onClick={this.displayModal} style={{cursor: 'pointer'}}>  CLOSE </button>
               </div>
-            </div> :
+            </form> :
             null
         }
       </div>
@@ -134,3 +179,6 @@ class AddReview extends React.Component {
 };
 
 export default AddReview;
+
+
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

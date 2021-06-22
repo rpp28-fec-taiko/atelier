3bfc92356df677a5e58bf660bb8b299bdec9e768
Farhan Ali <fahanali3193@gmail.com>
2021-06-22 00:57:10 -0700

    Added tests for bar and characteristic bar

---
+++ test/componentsTest/reviewsTest/characteristicBar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CharacteristicBar from '../../../client/src/components/reviews/characteristicBar.jsx';
+
+describe ('Bar', () => {
+  const wrapper = shallow(<CharacteristicBar filterReviews={() => {}}/>);
+
+  it ('checks if the component exists', () => {
+    expect(wrapper.find('.characteristic-bar').exists()).toBe(true);
+  });
+
+  it('should have 3 children', () => {
+    expect(wrapper.find('.characteristic-bar').children().length).toBe(3)
+  });
+});

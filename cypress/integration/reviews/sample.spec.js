7b5d6af9e446b8b0b28f33c4650a18bd51fd3f48
Farhan Ali <fahanali3193@gmail.com>
2021-07-01 00:45:05 -0700

    CHanged ip to localhost for my test

--- cypress/integration/reviews/sample.spec.js
+++ cypress/integration/reviews/sample.spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits Atelier', () => {
-    cy.visit('http://34.225.154.204')
+    cy.visit('http://localhost:3000')
 
     cy.get('.reviews-tile').should('have.length', 2)
     cy.contains('MORE REVIEWS').click();
